20338df22e3454b95945b7d8d9054512ef2964f9
gerinhoprogram <rogerio_furquim@hotmail.com>
2023-12-28 22:53:29 -0300

    templates

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home') 
+    return render(
+        request,
+        'home/index.html'
+    ) 
